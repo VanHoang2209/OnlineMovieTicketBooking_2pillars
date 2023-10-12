ccd49b508fc00f10ca513af762b6c6c0d4cc1ff9
Hoàng <vanhoang1231234@gmail.com>
2023-10-12 15:35:39 +0700

    Add project files.

---
+++ OnlineMovieTicketBooking_2pillars/Views/LoginMemberUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlineMovieTicketBooking_2pillars.Views
+{
+    public partial class LoginMemberUI : Form
+    {
+        public LoginMemberUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
